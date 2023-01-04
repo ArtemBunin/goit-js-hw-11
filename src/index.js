66f3a0ded3e039d3e87d47c37d96af7ca2d7b8d8
ArtemBunin <artembunin93@gmail.com>
2023-01-04 21:43:31 +0000

    update code

--- src/index.js
+++ src/index.js
@@ -24,39 +24,107 @@ async function onFormSubmit(ev) {
   searchQuery = ev.currentTarget.searchQuery.value.trim();
   clearpage();
   moreImg()
+//   try {
+//     await getImegs(searchQuery, page).then(
+//       ({hits,totalHits}) =>{
+       
+//         if (!searchQuery) {
+//             clearpage();
+//           Notify.failure('Write more correctly');
+//           return 
+//           } else{
+          
+//           }
+//         var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay :250})
+//         if (hits.length=== 0){
+//             Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+//             return 
+//         }
+//         if (searchQuery){  Notify.success(`Hooray! We found ${totalHits} images.`)};
+//         markupImages(hits);
+//         page +=1;
+//         enableBtn()
+//         pageHits +=hits.length
+        
+// if(pageHits>=totalHits){
+//     disabledBtn();
+  
+//     Notify.warning(`We're sorry, but you've reached the end of search results.`);
+//     return 
+// }
 
+//         } 
+//     );
+//   } catch (error) {
+//     console.error(error);
+//   }
 }
 
 async function moreImg() {
     try {
-        await getImegs(searchQuery, page).then(
-          ({hits,totalHits}) =>{
-            markupImages(hits);
-            page +=1;
-            enableBtn()
-            if (!searchQuery) {
-                clearpage()
-               Notify.failure('Write more correctly');
-               return 
+            await getImegs(searchQuery, page).then(
+              ({hits,totalHits}) =>{
                
-              }
-            var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay :250})
-            if (hits.length=== 0){
-                Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-                return 
-            }
-            pageHits +=hits.length;
-            if(pageHits>=totalHits){
-                disabledBtn();
-                Notify.warning(`We're sorry, but you've reached the end of search results.`);
-                return 
-            }
+                if (!searchQuery) {
+                    clearpage();
+                  Notify.failure('Write more correctly');
+                  return 
+                  } else{
+                  
+                  }
+                var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay :250})
+                if (hits.length=== 0){
+                    Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+                    return 
+                }
+                if (searchQuery){  Notify.success(`Hooray! We found ${totalHits} images.`)};
+                markupImages(hits);
+                page +=1;
+                enableBtn()
+                pageHits +=hits.length
+                
+        if(pageHits>=totalHits){
+            disabledBtn();
+          
+            Notify.warning(`We're sorry, but you've reached the end of search results.`);
+            return 
+        }
+        
+                } 
+            );
+          } catch (error) {
+            console.error(error);
+          }
+    // try {
+    //     await getImegs(searchQuery, page).then(
+    //       ({hits,totalHits}) =>{
+    //         markupImages(hits);
+    //         page +=1;
+    //         enableBtn()
+    //         if (!searchQuery) {
+    //             clearpage()
+    //            Notify.failure('Write more correctly');
+    //            return 
+               
+    //           }
+    //         var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay :250})
+    //         if (hits.length=== 0){
+    //             Notify.warning('Sorry, there are no images matching your search query. Please try again.');
+    //             return 
+    //         }
+    //         pageHits +=hits.length;
+    //         if(pageHits>=totalHits){
+    //             disabledBtn();
+    //             Notify.warning(`We're sorry, but you've reached the end of search results.`);
+    //             return 
+    //         }
        
-            } 
-        );
-      } catch (error) {
-        console.error(error);
-      }
+            
+    //         } 
+    //     );
+    //   } catch (error) {
+    //     console.error(error);
+    //   }
 }
 
 
