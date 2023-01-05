c24e856454a8c06d5a914b86d222c352745e475d
ArtemBunin <artembunin93@gmail.com>
2023-01-05 10:06:35 +0000

    fix bugs

--- src/index.js
+++ src/index.js
@@ -9,101 +9,73 @@ const ref = {
   loadMoreBtn: document.querySelector('.load-more'),
 };
 const gallerry = document.querySelector('.gallery');
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 let page = 1;
 let searchQuery = '';
 let pageHits = 0;
 
-
 ref.form.addEventListener('submit', onFormSubmit);
 
 ref.loadMoreBtn.addEventListener('click', moreImg);
-async function onFormSubmit(ev) {
+function onFormSubmit(ev) {
   ev.preventDefault();
- 
+
   searchQuery = ev.currentTarget.searchQuery.value.trim();
   clearpage();
-  moreImg()
+  moreImg();
 }
 
 async function moreImg() {
-    try {
-            await getImegs(searchQuery, page).then(
-              ({hits,totalHits}) =>{
-               
-                if (!searchQuery) {
-                    clearpage();
-                  Notify.failure('Write more correctly');
-                  return 
-                  }
-                
-                if (hits.length=== 0){
-                    disabledBtn()
-                    Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-                    return 
-                }
-                if (searchQuery){  Notify.success(`Hooray! We found ${totalHits} images.`)};
-               
-                if(pageHits>=totalHits){
-                    disabledBtn();
-                    Notify.warning(`We're sorry, but you've reached the end of search results.`);
-                    return 
-                }
-                page +=1;
-                enableBtn()
-                pageHits +=hits.length
-                markupImages(hits);
-                var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay :250})
-       
-        
-                } 
-            );
-          } catch (error) {
-            console.error(error);
-          }
-    // try {
-    //     await getImegs(searchQuery, page).then(
-    //       ({hits,totalHits}) =>{
-    //         markupImages(hits);
-    //         page +=1;
-    //         enableBtn()
-    //         if (!searchQuery) {
-    //             clearpage()
-    //            Notify.failure('Write more correctly');
-    //            return 
-               
-    //           }
-    //         var lightbox = new SimpleLightbox('.gallery a', { captionsData:'alt',captionDelay :250})
-    //         if (hits.length=== 0){
-    //             Notify.warning('Sorry, there are no images matching your search query. Please try again.');
-    //             return 
-    //         }
-    //         pageHits +=hits.length;
-    //         if(pageHits>=totalHits){
-    //             disabledBtn();
-    //             Notify.warning(`We're sorry, but you've reached the end of search results.`);
-    //             return 
-    //         }
-       
-            
-    //         } 
-    //     );
-    //   } catch (error) {
-    //     console.error(error);
-    //   }
+  try {
+    const { hits, totalHits } = await getImegs(searchQuery, page);
+
+    if (!searchQuery) {
+      clearpage();
+      Notify.failure('Write more correctly');
+      return;
+    }
+
+    if (hits.length === 0) {
+      disabledBtn();
+      Notify.warning(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+    if (searchQuery) {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
+
+    if (pageHits >= totalHits) {
+      disabledBtn();
+      Notify.warning(
+        `We're sorry, but you've reached the end of search results.`
+      );
+      return;
+    }
+    page += 1;
+    enableBtn();
+    pageHits += hits.length;
+    markupImages(hits);
+    lightbox.refresh();
+  } catch (error) {
+    console.error(error);
+  }
+  
 }
 
+function clearpage() {
+  gallerry.innerHTML = '';
+  page = 1;
+}
 
- function clearpage(){
-gallerry.innerHTML ='';
-page=1
- }
+function enableBtn() {
+  ref.loadMoreBtn.classList.remove('is-hidden');
+}
 
- function enableBtn(){
-    ref.loadMoreBtn.classList.remove('is-hidden')
- }
-
- function disabledBtn(){
-    ref.loadMoreBtn.classList.add('is-hidden')
-
- }
+function disabledBtn() {
+  ref.loadMoreBtn.classList.add('is-hidden');
+}
